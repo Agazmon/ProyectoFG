f4650778d0567e820bd1d0437ae5e5b76b01daf4
Agazmon <alexgazquezmonedero@gmail.com>
2020-10-06 16:21:19 +0200

    Inicialización de repositorio

---
+++ alexg/src/main/java/proyectogond/App.java
@@ -0,0 +1,12 @@
+package proyectogond;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String cadena = "Probando el debugger";
+        System.out.println(cadena);
+    }
+}
